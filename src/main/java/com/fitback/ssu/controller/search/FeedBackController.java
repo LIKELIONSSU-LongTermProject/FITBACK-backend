b6ed9c6902232e9cced8a9d08671cad40f0c04fd
seungmin Ryu <ojysep9987@naver.com>
2022-11-12 23:12:06 +0900

    API 등록 예정

---
+++ src/main/java/com/fitback/ssu/controller/search/FeedBackController.java
@@ -0,0 +1,18 @@
+package com.fitback.ssu.controller.search;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(value = "/feedback")
+public class FeedBackController {
+
+//    @GetMapping(value = "/new")
+//    @GetMapping(value = "/order/starttime")
+//    @GetMapping(value = "/order/endtime")
+//    @GetMapping(value = "/order/like")
+}

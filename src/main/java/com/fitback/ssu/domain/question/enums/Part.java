c03ce68fc5c37e874bdb4fbea647bbc48a69af4e
seungmin Ryu <ojysep9987@naver.com>
2022-11-12 23:05:43 +0900

    Feat: Part '백엔드, 프론트, 디자인, 기획'으로 구분

---
+++ src/main/java/com/fitback/ssu/domain/question/enums/Part.java
@@ -0,0 +1,28 @@
+package com.fitback.ssu.domain.question.enums;
+
+import com.fitback.ssu.domain.question.converter.CodeValue;
+
+public enum Part implements CodeValue {
+    DEV_BACK("B","DEV_BACK"),
+    DEV_FRONT("F","DEV_FRONT"),
+    DESIGN("D","DESIGN"),
+    PM("P","PM")
+    ;
+    private String code;
+    private String value;
+
+    Part(String code, String value) {
+        this.code = code;
+        this.value = value;
+    }
+
+    @Override
+    public String getCode() {
+        return code;
+    }
+
+    @Override
+    public String getValue() {
+        return value;
+    }
+}

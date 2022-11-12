19947029089e15faeaacbb4572aec54f30bdafee
seungmin Ryu <ojysep9987@naver.com>
2022-11-12 23:17:20 +0900

    Feat: tomcat 시작 전에 db 초기값 세팅 추가

---
+++ src/main/java/com/fitback/ssu/SsuApplication.java
@@ -0,0 +1,15 @@
+package com.fitback.ssu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.AbstractEnvironment;
+
+@SpringBootApplication
+public class SsuApplication {
+
+	public static void main(String[] args) {
+		System.setProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME,"local");
+		SpringApplication.run(SsuApplication.class, args);
+	}
+
+}

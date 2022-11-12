c7ef656933f851e987f7836c743c5b1a47005ebb
seungmin Ryu <ojysep9987@naver.com>
2022-11-12 22:59:32 +0900

    Chore: 프로젝트 기본 세팅

---
+++ src/test/java/com/fitback/ssu/SsuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitback.ssu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

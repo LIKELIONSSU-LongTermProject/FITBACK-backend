6da1965ecddbaad91019f99e0da4203fd0b2c1d4
seungmin Ryu <ojysep9987@naver.com>
2022-11-19 08:05:54 +0900

    Feat: 최근 등록된 API

--- src/main/java/com/fitback/ssu/controller/search/FeedBackController.java
+++ src/main/java/com/fitback/ssu/controller/search/FeedBackController.java
@@ -1,17 +1,25 @@
 package com.fitback.ssu.controller.search;
 
+import com.fitback.ssu.dto.question.QuestionInfoDto;
+import com.fitback.ssu.service.question.SearchService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(value = "/feedback")
 public class FeedBackController {
-
-//    @GetMapping(value = "/new")
+    private final SearchService searchService;
+    @GetMapping(value = "/new")
+    public ArrayList<QuestionInfoDto> designQuestions(){
+        return searchService.newQuestions();
+    }
 //    @GetMapping(value = "/order/starttime")
 //    @GetMapping(value = "/order/endtime")
 //    @GetMapping(value = "/order/like")

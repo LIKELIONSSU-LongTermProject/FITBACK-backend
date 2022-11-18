8636a8d8ca20f908afa49e64b80c6dcc08cc17eb
seungmin Ryu <ojysep9987@naver.com>
2022-11-19 08:06:33 +0900

    Feat: 메인페이지 개발,디자인,기획 탭 API

--- src/main/java/com/fitback/ssu/controller/search/MainController.java
+++ src/main/java/com/fitback/ssu/controller/search/MainController.java
@@ -1,16 +1,40 @@
 package com.fitback.ssu.controller.search;
 
+import com.fitback.ssu.dto.question.QuestionInfoDto;
+import com.fitback.ssu.service.question.SearchService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(value = "/main")
 public class MainController {
+    private final SearchService searchService;
+    @GetMapping(value = "/dev")
+    public ArrayList<QuestionInfoDto> devQuestions(
+            @RequestParam(value = "keyword",required = false,defaultValue = "dev") String keyword){
+//        log.info("keyword = {}",keyword);
+        return searchService.mainPageInfo(keyword);
+    }
 
+    @GetMapping(value = "/design")
+    public ArrayList<QuestionInfoDto> designQuestions(
+            @RequestParam(value = "keyword",required = false,defaultValue = "design") String keyword){
+        return searchService.mainPageInfo(keyword);
+    }
+
+    @GetMapping(value = "/pm")
+    public ArrayList<QuestionInfoDto> pmQuestions(
+            @RequestParam(value = "keyword",required = false,defaultValue = "pm") String keyword){
+        return searchService.mainPageInfo(keyword);
+    }
 //    @GetMapping(value = "/keyword")
 //    @GetMapping(value = "/part")
 //    @GetMapping(value = "/part/all")

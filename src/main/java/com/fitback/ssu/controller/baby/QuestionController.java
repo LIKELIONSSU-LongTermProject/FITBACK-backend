d608cace4d1c61e1854bfed3759b45355d1f7616
seungmin Ryu <ojysep9987@naver.com>
2022-11-19 18:30:24 +0900

    Docs: API 설명 주석 추가

--- src/main/java/com/fitback/ssu/controller/baby/QuestionController.java
+++ src/main/java/com/fitback/ssu/controller/baby/QuestionController.java
@@ -17,13 +17,25 @@ public class QuestionController {
 
     private final QuestionService questionService;
 
+    /**
+     * 질문 등록하기
+     * @param questionRegisterDto
+     */
     @PostMapping("")
     public void registerQuestion(@RequestBody QuestionRegisterDto questionRegisterDto){
         questionService.registerQuestion(questionRegisterDto);
     }
 
+    /**
+     * 메인 화면에서 질문하나 클릭했을 때 -> 자신의 것이면 아래쪽 블러x, 아니면 아래쪽 블러o
+     * @param qID
+     * @return
+     * @throws ParseException
+     */
     @GetMapping("/{qID}")
     public QuestionInfoDto getOneQuestion(@PathVariable Long qID) throws ParseException {
         return questionService.getOneQuestion(qID);
     }
+
+    //TODO 질문에 해당하는 답변 리스트
 }

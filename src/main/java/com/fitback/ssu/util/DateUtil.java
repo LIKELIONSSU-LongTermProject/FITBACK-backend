3d41123d39149a46ac124923184ac2ba45085fb5
seungmin Ryu <ojysep9987@naver.com>
2022-12-17 01:11:59 +0900

    Feat: LocalDateTime to String util

--- src/main/java/com/fitback/ssu/util/DateUtil.java
+++ src/main/java/com/fitback/ssu/util/DateUtil.java
@@ -4,10 +4,17 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
 @Slf4j
 public class DateUtil {
+    public static String dateTimeToString(LocalDateTime time){
+//        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy/MM/dd");
+//        return simpleDateFormat.format(time);
+        return time.format(DateTimeFormatter.ofPattern("yyyy/MM/dd"));
+    }
 
     public static Integer calDeadLine(String eTime, String sTime) throws ParseException {
         SimpleDateFormat beforeFormat = new SimpleDateFormat("yyyy/MM/dd");

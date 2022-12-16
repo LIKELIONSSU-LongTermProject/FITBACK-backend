664bd2b8f46e39d1c57b4abad81da78b185cc024
seungmin Ryu <ojysep9987@naver.com>
2022-12-17 01:17:20 +0900

    Feat: AWS S3 관련 세팅 추가

--- src/main/java/com/fitback/ssu/SsuApplication.java
+++ src/main/java/com/fitback/ssu/SsuApplication.java
@@ -2,12 +2,17 @@ package com.fitback.ssu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.AbstractEnvironment;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class SsuApplication {
-
+	static {
+		System.setProperty("com.amazonaws.sdk.disableEc2Metadata", "true");
+	}
 	public static void main(String[] args) {
-//		System.setProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME,"local");
+		System.setProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME,"local");
 		SpringApplication.run(SsuApplication.class, args);
 	}
 
